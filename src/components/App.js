d8cf845757733fccaab48ea16c60788a2d206c72
Aditya Singh <103933450+LINU00@users.noreply.github.com>
2025-11-05 23:50:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol key={"relativeList"}>
+        <li key={"relativeListItem1"}>Relative 1</li>
+        <li key={"relativeListItem2"}>Relative 2</li>
+        <li key={"relativeListItem3"}>Relative 3</li>
+        <li key={"relativeListItem4"}>Relative 4</li>
+      </ol>
     </div>
   )
 }
